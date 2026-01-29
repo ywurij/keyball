91e98bbf8b9a8c326941daeef4990cddfd5929f6
真鍋悠利 <ywurij10732@gmail.com>
2026-01-30 08:46:05 +0900

    Combo2_modified

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/my_custom_combo_2/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/my_custom_combo_2/keymap.c
@@ -20,7 +20,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "quantum.h"
 
-
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for default (VIA)
@@ -28,12 +27,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_MINUS ,
     LSFT_T(KC_TAB), KC_A , KC_S    , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_QUOT  ,
     KC_LCTL  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_INT1  ,
-              TG(7) , KC_LALT , LT(3, KC_LNG5) , LT(2,KC_BSPC) , LT(4,KC_SPACE),           RCTL_T(KC_SPACE), RSFT_T(KC_DEL), KC_NO , KC_NO  ,            TG(5)
+              KC_LWIN , KC_LALT , C(KC_ENT) , LT(2,KC_INT5) , LT(4,KC_SPACE),           RCTL_T(KC_SPACE), RSFT_T(KC_INT4), KC_NO , KC_NO  ,            TG(5)
   ),
 
   [1] = LAYOUT_universal(
-    KC_NO ,  KC_NO , KC_NO  , KC_NO  , KC_NO    , KC_NO    ,                                                 KC_B     , KC_BTN1  , KC_BTN3  , KC_BTN2  , KC_RCTL  , KC_RCTL ,
-    KC_NO ,  KC_NO , KC_NO  , KC_NO  , KC_LCTL  , KC_LSFT  ,                                                 SCRL_MO  , KC_BTN1  , KC_BTN3  , KC_BTN2  , KC_RSFT  , KC_RSFT ,
+    KC_NO ,  KC_NO , KC_NO  , KC_NO  , KC_NO    , KC_NO    ,                                                 KC_B     , KC_BTN1  , KC_BTN3  , KC_BTN2  , KC_RCTL  , KC_RSFT ,
+    KC_NO ,  KC_NO , KC_NO  , KC_NO  , KC_LCTL  , KC_LSFT  ,                                                 SCRL_MO  , KC_BTN1  , KC_BTN3  , KC_BTN2  , KC_RCTL  , KC_RSFT ,
     KC_NO ,  KC_NO , KC_NO  , KC_NO  , KC_V     , KC_B     ,                                                 KC_V     , KC_BTN1  , KC_BTN3  , KC_BTN2  , KC_NO    , KC_NO   ,
                   KC_NO  , KC_NO , KC_NO  , SCRL_MO  , KC_NO  ,                                       KC_NO  , MO(4)    , KC_NO    , KC_NO    ,            KC_PAST
   ),
@@ -42,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO  , S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5),                                                    S(KC_6)  , S(KC_7)  , S(KC_8)  , S(KC_9)  , KC_LBRC  , KC_EQL   ,
     KC_NO  , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                                                    KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , KC_PPLS  ,
     KC_NO  , KC_SCLN, KC_QUOT, KC_COMM, S(KC_8), KC_RBRC,                                                    KC_NUHS  , S(KC_9)  , KC_DOT   , KC_PDOT  , KC_PSLS  , KC_PMNS  ,
-                  KC_INT1, KC_INT3, KC_NO  , KC_NO  , KC_NO  ,                                      MO(6)  , KC_RSFT  , KC_NO    , KC_NO    ,            KC_PAST
+                  KC_INT1, KC_INT3, KC_NO  , KC_NO  , KC_NO  ,                                      MO(3)  , KC_RSFT  , KC_NO    , KC_NO    ,            KC_PAST
   ),
 
   [3] = LAYOUT_universal(
@@ -54,8 +53,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [4] = LAYOUT_universal(
     KC_NO  , KC_NO  , KC_NO  , KC_UP  , KC_NO   , KC_NO  ,                                                   KC_H   , S(KC_A), S(KC_S), S(KC_C), KC_G   , KC_F11 ,
-    KC_NO  , KC_HOME, KC_LEFT, KC_DOWN, KC_RIGHT, KC_END ,                                                   KC_1   , KC_S   , KC_X   , KC_D   , KC_E   , KC_Z   ,
-    KC_NO  , C(KC_Z), C(KC_X), C(KC_C), C(KC_V) , KC_NO  ,                                                   KC_2   , KC_Q   , KC_F   , KC_NO  , KC_NO  , KC_SPACE,
+    KC_NO  , KC_HOME, KC_LEFT, KC_DOWN, KC_RIGHT, KC_END ,                                                   KC_V   , KC_S   , KC_X   , KC_D   , KC_E   , KC_Z   ,
+    KC_NO  , C(KC_Z), C(KC_X), C(KC_C), C(KC_V) , KC_NO  ,                                                   KC_B   , KC_Q   , KC_F   , KC_1  , KC_2  , KC_SPACE,
                   KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,                                      KC_RCTL, KC_RSFT, KC_NO  , KC_NO  ,          KC_NO
   ),
 
@@ -74,7 +73,7 @@ enum combos {
   CV_DOWN,
   FG_END,
   AS_HOME,
-  ASDF_TAB,
+  WER_TAB,
   RT_ESC,
   JK_ENT,
   SDF_BSPC,
@@ -87,7 +86,7 @@ const uint16_t PROGMEM sd_left_combo[] = {KC_S, KC_D, COMBO_END};
 const uint16_t PROGMEM cv_down_combo[] = {KC_C, KC_V, COMBO_END};
 const uint16_t PROGMEM fg_end_combo[] = {KC_F, KC_G, COMBO_END};
 const uint16_t PROGMEM as_home_combo[] = {KC_A, KC_S, COMBO_END};
-const uint16_t PROGMEM asdf_tab_combo[] = {KC_A, KC_S, KC_D, KC_F, COMBO_END};
+const uint16_t PROGMEM wer_tab_combo[] = {KC_W, KC_E, KC_R, COMBO_END};
 const uint16_t PROGMEM rt_esc_combo[] = {KC_R, KC_T, COMBO_END};
 const uint16_t PROGMEM jk_ent_combo[] = {KC_J, KC_K, COMBO_END};
 const uint16_t PROGMEM sdf_bspc_combo[] = {KC_S, KC_D, KC_F, COMBO_END};
@@ -100,7 +99,7 @@ combo_t key_combos[] = {
   [CV_DOWN] = COMBO(cv_down_combo, KC_DOWN),
   [FG_END] = COMBO(fg_end_combo, KC_END),
   [AS_HOME] = COMBO(as_home_combo, KC_HOME),
-  [ASDF_TAB] = COMBO(asdf_tab_combo, KC_TAB),
+  [WER_TAB] = COMBO(wer_tab_combo, KC_TAB),
   [RT_ESC] = COMBO(rt_esc_combo, KC_ESC),
   [JK_ENT] = COMBO(jk_ent_combo, KC_ENT),
   [SDF_BSPC] = COMBO(sdf_bspc_combo, KC_BSPC),
@@ -110,18 +109,18 @@ combo_t key_combos[] = {
 // clang-format on
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    // Auto enable scroll mode when the highest layer is 3
-    keyball_set_scroll_mode(get_highest_layer(state) == 3);
-    return state;
+  // Auto enable scroll mode when the highest layer is 3
+  keyball_set_scroll_mode(get_highest_layer(state) == 3);
+  return state;
 }
 
 #ifdef OLED_ENABLE
 
-#    include "lib/oledkit/oledkit.h"
+#include "lib/oledkit/oledkit.h"
 
 void oledkit_render_info_user(void) {
-    keyball_oled_render_keyinfo();
-    keyball_oled_render_ballinfo();
-    keyball_oled_render_layerinfo();
+  keyball_oled_render_keyinfo();
+  keyball_oled_render_ballinfo();
+  keyball_oled_render_layerinfo();
 }
 #endif
